224dde7f4d24c3f5793281707cf9f6d316305930
adeep <adeepmah@gmail.com>
2025-02-16 17:19:23 +0530

    16/02/2025-if/else

---
+++ src/ex_06_if_else_condition/Lab007_Grade_number.java
@@ -0,0 +1,24 @@
+package ex_06_if_else_condition;
+
+import java.util.Scanner;
+
+public class Lab007_Grade_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the score");
+        float score = sc.nextFloat();
+        if (score >= 90 && score <= 100) {
+            System.out.println("you got the A grade");
+        } else if (score <= 89 && score >= 80) {
+            System.out.println("you got the B grade");
+        } else if (score <= 79 && score >= 70) {
+            System.out.println("you got the C grade");
+        } else if (score <= 69 && score >= 60) {
+            System.out.println("you got the D grade");
+        } else {
+            System.out.println("you are fail");
+        }
+
+
+    }
+}
